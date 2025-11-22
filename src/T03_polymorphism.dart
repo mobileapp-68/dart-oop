e314379a7aef9bf9c5586b0e610185a92059c68a
Nirand Pisutha-Arnond <nnnpooh@gmail.com>
2025-11-22 09:26:07 +0700

    Add implementation of polymorphism and database examples in Dart

---
+++ src/T03_polymorphism.dart
@@ -0,0 +1,81 @@
+void main() {
+  Shape circle = Circle(5);
+  print('Area of Circle: ${circle.area()}'); // Output: Area of Circle: 78.5
+
+  Shape rectangle = Rectangle(4, 6);
+  print(
+    'Area of Rectangle: ${rectangle.area()}',
+  ); // Output: Area of Rectangle: 24
+
+  // Polymorphic behavior
+  List<Shape> shapes = [Circle(3), Rectangle(2, 5), Circle(7)];
+  for (var shape in shapes) {
+    print('Shape area: ${shape.area()}');
+  }
+
+  // Database example
+  // String dbType = 'MySQL';
+  String dbType = 'MongoDB';
+  Database db = dbType == 'MySQL' ? MySQLDatabase() : MongoDBDatabase();
+  db.connect();
+  db.getData();
+}
+
+abstract class Shape {
+  double area();
+}
+
+class Circle extends Shape {
+  double radius;
+
+  Circle(this.radius);
+
+  @override
+  double area() {
+    return 3.14 * radius * radius;
+  }
+}
+
+class Rectangle extends Shape {
+  double width;
+  double height;
+
+  Rectangle(this.width, this.height);
+
+  @override
+  double area() {
+    return width * height;
+  }
+}
+
+abstract class Database {
+  void connect() {
+    print('Connecting to database');
+  }
+
+  void getData();
+}
+
+class MySQLDatabase extends Database {
+  @override
+  void connect() {
+    print('Connecting to MySQL database');
+  }
+
+  @override
+  void getData() {
+    print('Getting data from MySQL database');
+  }
+}
+
+class MongoDBDatabase extends Database {
+  @override
+  void connect() {
+    print('Connecting to MongoDB database');
+  }
+
+  @override
+  void getData() {
+    print('Getting data from MongoDB database');
+  }
+}
