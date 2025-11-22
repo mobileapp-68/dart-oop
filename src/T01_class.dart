fafe55d07de02fa1f7fa2c7e5b4abadf114081fc
Nirand Pisutha-Arnond <nnnpooh@gmail.com>
2025-11-22 07:04:14 +0700

    Refactor Student class implementations: update constructors and improve variable handling

--- src/T01_class.dart
+++ src/T01_class.dart
@@ -1,15 +1,32 @@
 void main() {
-  StudentA s1 = StudentA();
+  var s1 = StudentA();
   print(s1.gpa);
 
-  StudentB s2 = StudentB();
-  // print(s2._gpa);
+  var s2 = StudentB();
+  // print(s2._gpa);  // This is allowed only within the same library.
   print(s2.gpa);
   s2.gpa = 3.8;
   print(s2.gpa);
 
-  var s3 = StudentC("John", gpa: 4.0);
-  print("${s3.name}'s GPA is ${s3.gpa}");
+  var s3 = StudentC(gpa: 4.0);
+  print(s3.gpa);
+
+  var s4 = StudentD(gpa: 3.5);
+  print(s4.gpa);
+
+  var s5 = StudentE(gpa: 3.9, name: "Alice");
+  print(s5.gpa);
+  print(s5.name);
+
+  var s6 = StudentF(gpa: 4.0);
+  print(s6.gpa);
+  // This will cause an error because gpa is final.
+  // s6.gpa = 3.5;
+
+  var s7 = StudentG(gpa: 3.7);
+  print(s7.gpa);
+  var s8 = StudentG.positional(3.9);
+  print(s8.gpa);
 }
 
 class StudentA {
@@ -31,14 +48,48 @@ class StudentB {
   // this._gpa = value;
   // }
   set gpa(double gpa) => _gpa = gpa;
+
+  // You can also define regular methods
+  // void setGpa(double gpa) {
+  //   _gpa = gpa;
+  // }
 }
 
 class StudentC {
-  double gpa;
-  String? name;
+  // Need nullable type to allow uninitialized variable
+  double? gpa;
 
-  // Student({required double gpa, String? name}) {
-  //   this._gpa = gpa;
-  // }
-  StudentC(this.name, {required this.gpa});
+  StudentC({required double gpa}) {
+    this.gpa = gpa;
+  }
+}
+
+class StudentD {
+  // Non-nullable variable with constructor parameter
+  double gpa;
+
+  StudentD({required this.gpa});
+}
+
+class StudentE {
+  double gpa;
+  String name;
+
+  StudentE({required this.gpa, this.name = ""});
+}
+
+class StudentF {
+  // Final variable, must be initialized and cannot be changed later.
+  final double gpa;
+
+  StudentF({required this.gpa});
+}
+
+class StudentG {
+  // Final variable, must be initialized and cannot be changed later.
+  final double gpa;
+
+  StudentG({required this.gpa});
+  // Named constructor with positional parameter
+  StudentG.positional(this.gpa);
 }
